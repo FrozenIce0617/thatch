c70a16c329a879377e0558794544fbbc3b090179
FrozenIce0617 <prosdev087@gmail.com>
2022-08-10 15:03:36 -0500

    feat: update folder structure

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { AppShell, Container, Navbar, Center, Tooltip, UnstyledButton, Stack } from '@mantine/core';
+import { TablerIcon, IconLogout } from '@tabler/icons';
+
+import Logo from 'assets/images/logo.svg';
+
+import { sidebarData } from './sidebarData';
+import useStyles from './Layout.styles';
+
+interface NavbarLinkProps {
+  icon: TablerIcon;
+  label: string;
+  active?: boolean;
+  onClick?(): void;
+}
+
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+  const { classes, cx } = useStyles();
+
+  return (
+    <Tooltip label={label} position="right" transitionDuration={0}>
+      <UnstyledButton className={cx(classes.link, { [classes.active]: active })} onClick={onClick}>
+        <Icon stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+export const Sidebar = () => {
+  const { classes } = useStyles();
+  const [active, setActive] = useState(0);
+
+  return (
+    <Navbar className={classes.navbar} width={{ base: 80 }} p="md">
+      <Center>
+        <Image src={Logo} width={30} height={30} />
+      </Center>
+      <Navbar.Section grow mt={50}>
+        <Stack justify="center" spacing={4}>
+          {sidebarData.map((link, index) => (
+            <NavbarLink
+              {...link}
+              key={link.label}
+              active={index === active}
+              onClick={() => setActive(index)}
+            />
+          ))}
+        </Stack>
+      </Navbar.Section>
+      <Navbar.Section>
+        <Stack justify="center" spacing={0}>
+          <NavbarLink icon={IconLogout} label="Logout" />
+        </Stack>
+      </Navbar.Section>
+    </Navbar>
+  );
+};
+
+export const Layout = ({ children }: { children: React.ReactNode }) => (
+  <AppShell padding="md" navbar={<Sidebar />}>
+    <Container>{children}</Container>
+  </AppShell>
+);
---
+++ src/components/Layout/sidebarData.ts
@@ -0,0 +1,8 @@
+import { IconHome2, IconSearch, IconMap, IconBooks } from '@tabler/icons';
+
+export const sidebarData = [
+  { icon: IconHome2, label: 'Home' },
+  { icon: IconSearch, label: 'Explore' },
+  { icon: IconMap, label: 'Your Map' },
+  { icon: IconBooks, label: 'Your Library' },
+];
